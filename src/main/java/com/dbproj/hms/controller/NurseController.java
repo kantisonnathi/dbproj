a369fb9cb0abb6339928e965fc7d1ebb101202ec
unknown <mohanasrujana@gmail.com>
2021-03-31 04:43:37 +0530

    Nurseq

--- src/main/java/com/dbproj/hms/controller/NurseController.java
+++ src/main/java/com/dbproj/hms/controller/NurseController.java
@@ -1,4 +1,137 @@
 package com.dbproj.hms.controller;
+import com.dbproj.hms.model.Nurse;
+import com.dbproj.hms.model.Employee;
+import com.dbproj.hms.repository.NurseRepository;
+import com.dbproj.hms.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import java.sql.SQLException;
+import java.util.List;
 
+@Controller
+@CrossOrigin
 public class NurseController {
+
+    @Autowired
+    NurseRepository nurseRepository;
+    @Autowired
+    EmployeeRepository employeeRepository;
+    @GetMapping("/findNurseByID")
+    public String getByID(ModelMap model) {
+        Integer id = 0;
+        model.put("id",id);
+        return "nurse/findingNurseID";
+    }
+
+    @PostMapping("/findNurseByID")
+    public String postID(Integer id) {
+                return "redirect:/nur/" + id ;
+    }
+
+    @GetMapping("/findNurseByName")
+    public String getName(ModelMap model) {
+        String name = "";
+        model.put("name",name);
+        return "Nurse/findingNurByName";
+    }
+
+    @PostMapping("/findNurseByName")
+    public String postName(String name, ModelMap model) {
+        List<Nurse> nurses;
+        try {
+            nurses = nurseRepository.findByName(name);
+        } catch (SQLException e) {
+            return "system/error";
+        }
+        model.put("nurses",nurses);
+        model.put("title","Querying By Name");
+        return "nurse/listResults";
+    }
+
+
+    @GetMapping("/nur/{nurID}")
+    public String getNurPage(@PathVariable("nurID") Integer NurID, ModelMap model) {
+        Nurse nurse;
+        Employee employee;
+        try {
+            nurse = nurseRepository.findByID(NurID);
+            employee = employeeRepository.findByID(nurse.getEmpID());
+        } catch (SQLException e) {
+            return "system/error";
+        }
+        model.put("nurse",nurse);
+        model.put("employee", employee);
+        return "nurse/nurse";
+    }
+
+
+    @GetMapping("/addnurse")
+    public String getformpage(ModelMap modelMap){
+        Nurse nurse= new Nurse();
+
+        modelMap.put("nurse",nurse);
+        return "nurse/newnurse";
+    }
+
+
+    @PostMapping("/addnurse")
+    public String postaddition( Nurse nurse,ModelMap modelMap) {
+        try {
+            nurse.setAuthorization();
+            nurse.setVerify(1);
+            nurseRepository.save(nurse);
+        }
+        catch (SQLException e) {
+            return "system/error";
+        }
+        return "main";
+    }
+
+    @RequestMapping("/nur/{nurid}/delete")
+    @Transactional
+    public String deletenur(@PathVariable("nurid") int nurid) throws SQLException {
+        try {
+            Nurse nurse=nurseRepository.findByID(nurid);
+            nurseRepository.delete(nurse);
+        }
+        catch(SQLException e) {
+            return "system/error";
+        }
+        return "main";
+    }
+
+    @GetMapping("/nur/{nurid}/update")
+    public String getUpdatingNMP(@PathVariable("nurid") Integer nurid, ModelMap modelMap) {
+        Nurse nurse;
+        Employee emp;
+        try {
+            nurse = nurseRepository.findByID(nurid);
+            emp = employeeRepository.findByID(nurse.getEmpID());
+        } catch (Exception e) {
+            return "system/error";
+        }
+        modelMap.put("nurse",nurse);
+        modelMap.put("emp",emp);
+        modelMap.put("empID",emp.getID());
+        return "nurse/update";
+    }
+
+    @PostMapping("/nur/{nurid}/update")
+    public String postUpdatingNMP(Nurse nurse, Employee emp, Integer empID,ModelMap modelMap) {
+        try {
+            nurse.setAuthorization();
+            nurse.setVerify(1);
+            nurse.setEmpID(empID);
+            nurse = nurseRepository.update(nurse);
+
+             } catch (Exception e) {
+            return "system/error";
+        }
+        return "redirect:/nur/"+nurse.getID();
+    }
+
 }
+
---
+++ src/main/java/com/dbproj/hms/repository/NurseRepository.java
@@ -0,0 +1,63 @@
+package com.dbproj.hms.repository;
+
+
+import com.dbproj.hms.model.Employee;
+import com.dbproj.hms.model.Nurse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.sql.SQLException;
+import java.util.List;
+@Component
+public class NurseRepository {
+    @Autowired
+    private JdbcTemplate jdbcTemplate1;
+
+    public Nurse findByID(@org.jetbrains.annotations.NotNull Integer ID) throws DataAccessException, SQLException {
+        String query = "select * from nurse where nurseID=" + ID.toString();
+        List<Nurse> listn = jdbcTemplate1.query(query, new NurseRowMapper());
+        if (listn.isEmpty())
+        {
+            return null;
+        }
+        return listn.get(0);
+    }
+
+    public List<Nurse> findByName(String name) throws DataAccessException,SQLException{
+        String query = "select * from nurse N where N.empID in (select EmpID" +
+                " from employee where EmpName='" + name + "')";
+        return jdbcTemplate1.query(query,new NurseRowMapper());
+    }
+
+    //public List<Nurse> findBySpeciality(String speciality) throws DataAccessException, SQLException {
+      //  String query = "select * from doctor where speciality='" + speciality + "'";
+        //return jdbcTemplate1.query(query, new NurseRowMapper());
+    //}
+
+    public void delete(Nurse nurse) throws DataAccessException,SQLException{
+        String query="delete from employee  where empID="+nurse.getEmpID();
+        jdbcTemplate1.update(query);
+    }
+
+    public void save(Nurse nurse) throws DataAccessException,SQLException {
+        String query="insert into employee(empname,username,password,gender,salary,phno,email,address,authorization ,verify) values('"+ nurse.getName()+"','"+nurse.getUsername()+"','"+nurse.getPassword()+"','"+nurse.getGender()+"','"
+                +nurse.getSalary()+"','"+nurse.getPhoneNumber()+"','"+nurse.getEmail()+"','"+nurse.getAddress()+"','"+nurse.getAuthorization()+"','"+nurse.getVerify()+"')";
+        jdbcTemplate1.update(query);
+        query="select * from employee where username= '"+nurse.getUsername()+"'";
+        List<Employee> l=jdbcTemplate1.query(query,new EmployeeRowMapper());
+        Employee nur=l.get(0);
+        System.out.println(nurse.toString());
+        query="Insert into nurse(nurseID,empID) values('"+nurse.getID()+"','"+nurse.getEmpID() +"')";
+        jdbcTemplate1.update(query);
+    }
+
+    public Nurse  update( Nurse nurse) throws DataAccessException {
+        String query = "update employee set EmpName='" + nurse.getName() + "', username='" + nurse.getUsername() + "', gender='" + nurse.getGender() + "', salary=" + nurse.getSalary() + ", phno='" +
+                nurse.getPhoneNumber() + "', email='" + nurse.getEmail() + "', address='" + nurse.getAddress() + "', authorization='" +
+                nurse.getAuthorization() + "', verify=" + nurse.getVerify() + " where EmpID=" + nurse.getEmpID();
+        jdbcTemplate1.update(query);
+        return nurse;
+    }
+}
