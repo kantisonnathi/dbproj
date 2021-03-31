a530fd5666acf9e1015e8fee48a039512e2be8a7
Shreya Guda <73335846+shreya-guda@users.noreply.github.com>
2021-03-31 15:40:04 +0530

    nurse row mapper

--- src/main/java/com/dbproj/hms/repository/NurseRowMapper.java
+++ src/main/java/com/dbproj/hms/repository/NurseRowMapper.java
@@ -1,4 +1,17 @@
 package com.dbproj.hms.repository;
 
-public class NurseRowMapper {
+import com.dbproj.hms.model.Employee;
+import com.dbproj.hms.model.Nurse;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class NurseRowMapper implements RowMapper<Nurse> {
+    @Override
+    public Nurse mapRow(ResultSet resultSet, int i) throws SQLException {
+        return new Nurse(resultSet.getInt("nurseID"),
+                resultSet.getInt("empID"));
+    }
 }
+
