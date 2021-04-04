394a8da3c07bf067d70d0119dab88fc2e12e248a
sahithi <sahithi02.ra@gmail.com>
2021-04-04 11:29:30 +0530

    With roles

--- src/main/java/com/dbproj/hms/SpringSecurity/SecurityConfiguration.java
+++ src/main/java/com/dbproj/hms/SpringSecurity/SecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
